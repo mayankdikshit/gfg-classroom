e605fb284749cb874a6cd0d3d547344c91c6cffa
mayankdikshit <mayankdikshit711@gmail.com>
2025-04-02 12:30:23 +0530

    started npm

--- WD-14/script.js
+++ WD-14/script.js
@@ -1,20 +1,23 @@
-// const str = 'my name is mayank'
+// // const str = 'my name is mayank'
 
-// const reverseStr = (str) => {
-//     const newStr = str.split(' ').reverse().join(' ')
-//     return newStr
+// // const reverseStr = (str) => {
+// //     const newStr = str.split(' ').reverse().join(' ')
+// //     return newStr
+// // }
+// // console.log(reverseStr(str))
+
+// // Sentence case 
+
+// const upperStr = (str) => {
+//     const newStr = str.split(' ')
+//     console.log(newStr)
+
+//     const uppercaseStr = newStr.map(word => word[0].toUpperCase())
+//     return uppercaseStr
 // }
-// console.log(reverseStr(str))
 
-// Sentence case 
+// const str = 'hello geeks'
+// console.log(upperStr(str))
 
-const upperStr = (str) => {
-    const newStr = str.split(' ')
-    console.log(newStr)
 
-    const uppercaseStr = newStr.map(word => word[0].toUpperCase())
-    return uppercaseStr
-}
 
-const str = 'hello geeks'
-console.log(upperStr(str))
